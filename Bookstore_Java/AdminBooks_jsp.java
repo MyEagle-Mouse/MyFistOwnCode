9e929569edc18de9cf46171008f463c055ba08f1
MyEagle-Mouse <2375660261@qq.com>
2020-07-17 14:42:45 +0800

    提交代码

--- Bookstore_Java/AdminBooks_jsp.java
+++ Bookstore_Java/AdminBooks_jsp.java
@@ -12,6 +12,7 @@ public class AdminBooks_jsp extends HttpJspBase {
   static final String CRLF = "\r\n";
 
   static final int UNDEFINT=Integer.MIN_VALUE;
+  static final int adss = 8;
 
   static final int adText = 1;
   static final int adDate = 2;
